8c80b523ccd07dac003f52529add04969434b141
neer2808 <uic.neerajkhanna@gmail.com>
2022-02-02 19:44:59 +0530

    sorting of array by using the arrays.sort method

--- src/ArrayAssign6differentway.java
+++ src/ArrayAssign6differentway.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+
 public class ArrayAssign6differentway {
+  public static void main(String[] args) {
+    int arr[]= {12,3,44,56,2,67};
+    // sorting of array by using the Arrays.sort() method
+    // which is a predefined method
+
+    Arrays.sort(arr);
+    for (int i = 0; i <arr.length ; i++) {
+      System.out.println(arr[i]);
+    };
+  }
 }
