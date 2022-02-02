b3cfd1fa55bce3ee9d5f43be6fcdaccc203c1001
neer2808 <uic.neerajkhanna@gmail.com>
2022-02-02 23:50:01 +0530

    implement Interface Assignment

---
+++ src/InterfaceAssign1.java
@@ -0,0 +1,4 @@
+
+
+public class InterfaceAssign1 {
+}
---
+++ src/live/Test.java
@@ -0,0 +1,19 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+  public static void main(String[] args) {
+    Veena obj = new Veena();
+    obj.play();
+    Saxophone obj1 = new Saxophone();
+    obj.play();
+    Playable pobj ;
+    pobj = new Veena();
+    pobj.play();
+    pobj = new Saxophone();
+    pobj.play();
+  }
+}
